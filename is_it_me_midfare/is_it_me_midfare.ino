2971a2262a13f666c2799a581d9a9108898b0371
Damien Alexandre <mobman02@gmail.com>
2012-11-01 18:40:14 +0100

    Add mezzo and play music!!

--- is_it_me_midfare/is_it_me_midfare.ino
+++ is_it_me_midfare/is_it_me_midfare.ino
@@ -23,8 +23,9 @@ Base code from Adafruit:
 
 #define IRQ     (2)
 #define RESET   (3)  // Not connected by default on the NFC Shield
-#define OK_PIN  (11)
 #define NOK_PIN (10)
+#define OK_PIN  (11)
+#define MEZ_PIN (9)
 
 Adafruit_NFCShield_I2C nfc(IRQ, RESET);
 
@@ -36,14 +37,18 @@ void setup(void) {
   nfc.begin();
   pinMode(OK_PIN, OUTPUT);    
   pinMode(NOK_PIN, OUTPUT);   
+  pinMode(MEZ_PIN, OUTPUT);
 
-  // Auto-test LED's :)
+  // Auto-test LED's and Mezzo :)
   Serial.println("Testing LEDs");
   digitalWrite(OK_PIN, HIGH);
   digitalWrite(NOK_PIN, HIGH);
   delay(1000);
   digitalWrite(OK_PIN, LOW); 
   digitalWrite(NOK_PIN, LOW);
+  
+  // Play a note
+  playNote('c', 500);
 
   // configure board to read RFID tags
   nfc.SAMConfig();
@@ -89,6 +94,10 @@ void loop(void) {
 void itsme() {
   digitalWrite(OK_PIN, HIGH);
   digitalWrite(NOK_PIN, LOW);
+  
+  int beats[] = { 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3 };
+  playMelody(12, "g adad c g d", beats);
+  
   delay(1000);
   digitalWrite(OK_PIN, LOW);
 }
@@ -96,6 +105,47 @@ void itsme() {
 void itsnotme() {
   digitalWrite(NOK_PIN, HIGH);
   digitalWrite(OK_PIN, LOW);
+  
+  int beats[] = { 1, 1, 1, 1, 1, 1, 1, 1 };
+  playMelody(8, "Cbagfedc", beats);
+  
   delay(1000);
   digitalWrite(NOK_PIN, LOW);
+}
+
+void playMelody(int length, char notes[], int beats[]) 
+{
+  int tempo = 130;
+  
+  for (int i = 0; i < length; i++) {
+    if (notes[i] == ' ') {
+      delay(beats[i] * tempo); // rest
+    } else {
+      playNote(notes[i], beats[i] * tempo);
+    }
+    
+    // pause between notes
+    delay(tempo / 3); 
+  }
+}
+
+void playTone(int tone, int duration) {
+  for (long i = 0; i < duration * 1000L; i += tone * 2) {
+    digitalWrite(MEZ_PIN, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(MEZ_PIN, LOW);
+    delayMicroseconds(tone);
+  }
+}
+
+void playNote(char note, int duration) {
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
+  
+  // play the tone corresponding to the note name
+  for (int i = 0; i < 8; i++) {
+    if (names[i] == note) {
+      playTone(tones[i], duration);
+    }
+  }
 }
