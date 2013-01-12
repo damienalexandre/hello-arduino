927b38ea416df125fdd0b6dfc6c9b49cd45728b7
Damien Alexandre <mobman02@gmail.com>
2013-01-12 19:43:02 +0100

    M&M's distributor is ready ;D

--- servo/servo.ino
+++ servo/servo.ino
@@ -2,20 +2,37 @@
  
 Servo myservo;  // create servo object to control a servo 
                 // a maximum of eight servo objects can be created 
- 
-int pos = 0;    // variable to store the servo position 
- 
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+ int buttonState = 0;         // variable for reading the pushbutton status
 void setup() 
 { 
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+  pinMode(buttonPin, INPUT);    
+ // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);     
 } 
  
  
 void loop() 
 { 
-  myservo.write(170);
-  delay(1500);
+  
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+  
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {    
+    digitalWrite(ledPin, HIGH);   
+    myservo.write(170);
+    delay(1500);
 
-  myservo.write(1);              // tell servo to go to position in variable 'pos' 
-  delay(1500);
+    myservo.write(10);              // tell servo to go to position in variable 'pos' 
+    delay(500);
+    digitalWrite(ledPin, LOW); 
+  } 
+  else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW); 
+  }
 }
